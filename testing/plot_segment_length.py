bf748bb9f6e85115cbdab056d588a4a3bc829bca
JannesSp <janspang@gmx.de>
2022-06-28 16:34:18 +0200

    update

--- testing/plot_segment_length.py
+++ testing/plot_segment_length.py
@@ -22,11 +22,11 @@ for id in ids:
 
 # ============================ MEANS ============================
 # the bins should be of integer width, because poisson is an integer distribution
-plt.hist(ms, bins=60, density=True, label='means')
+plt.hist(ms, bins=60, label='means')
 axes = plt.gca()
 y_min, y_max = axes.get_ylim()
-plt.title('epinano_nomod_mean_segment_lengths_distribution.png')
-plt.vlines(np.median(ms), ymin = y_min, ymax=y_max)
+plt.title('EpiNano unmodified mean segment lengths distribution')
+plt.vlines(np.median(ms), ymin = y_min, ymax=y_max, color = 'red')
 plt.text(np.median(ms), y_max, 'median: ' + str(np.median(ms)))
 
 # find minimum and maximum of xticks, so we know
@@ -35,8 +35,8 @@ xt = plt.xticks()[0]
 xmin, xmax = min(xt), max(xt)  
 lnspc = np.linspace(xmin, xmax, len(ms))
 
-plt.plot(lnspc, stats.poisson.pmf(lnspc, np.mean(ms)), label="poisson")
-plt.plot(lnspc, stats.nbinom.pmf(lnspc, np.mean(ms), np.mean(ms)/2), label="nbinom")
+plt.plot(lnspc, stats.poisson.pmf(lnspc, np.mean(ms)), 'go', markersize=9, label="poisson")
+plt.plot(lnspc, stats.nbinom.pmf(lnspc, np.mean(ms), np.mean(ms)/2), 'go', markersize=9, label="nbinom")
 
 plt.legend()
 plt.savefig('epinano_nomod_mean_segment_lengths_distribution.png')
@@ -45,11 +45,11 @@ plt.close()
 
 # ============================ MEDIAN ============================
 # the bins should be of integer width, because poisson is an integer distribution
-entries, bin_edges, patches = plt.hist(md, bins=60, density=True, label='medians')
+entries, bin_edges, patches = plt.hist(md, bins=60, label='medians')
 axes = plt.gca()
 y_min, y_max = axes.get_ylim()
-plt.title('epinano_nomod_median_segment_lengths_distribution.png')
-plt.vlines(np.median(md), ymin = y_min, ymax=y_max)
+plt.title('EpiNano unmodified median segment lengths distribution')
+plt.vlines(np.median(md), ymin = y_min, ymax=y_max, color = 'red')
 plt.text(np.median(md), y_max, 'median: ' + str(np.median(md)))
 
 # find minimum and maximum of xticks, so we know
@@ -58,8 +58,8 @@ xt = plt.xticks()[0]
 xmin, xmax = min(xt), max(xt)  
 lnspc = np.linspace(xmin, xmax, len(md))
 
-plt.plot(lnspc, stats.poisson.pmf(lnspc, np.mean(md)), label="poisson")
-plt.plot(lnspc, stats.nbinom.pmf(lnspc, np.mean(md), np.mean(md)/2), label="nbinom")
+plt.plot(lnspc, stats.poisson.pmf(lnspc, np.mean(md)), 'go', markersize=9, label="poisson")
+plt.plot(lnspc, stats.nbinom.pmf(lnspc, np.mean(md), np.mean(md)/2), 'go', markersize=9, label="nbinom")
 
 plt.legend()
 plt.savefig('epinano_nomod_median_segment_lengths_distribution.png')
