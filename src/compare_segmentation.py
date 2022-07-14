f3c2b4835bb726b60c2e0a77adda336f22650e35
JannesSP <janspang@gmx.de>
2022-07-14 15:29:33 +0200

    update

--- src/compare_segmentation.py
+++ src/compare_segmentation.py
@@ -18,78 +18,157 @@ def parse() -> Namespace:
     parser.add_argument('sim_fast5', type = str, help='FAST5 file with simulated reads')
     parser.add_argument('nanopolish_segmentation_fast5', type = str, help='FAST5 file with nanopolish segmentation')
     parser.add_argument('fastq', type = str, help = 'Basecalled reads')
+    parser.add_argument('outpath', type = str, help = 'Path to output files')
 
     return parser.parse_args()
 
-def compare(sim5 : h5py.File, nano5 : h5py.File, fastqs : dict) -> pd.DataFrame:
+def compare(sim5 : h5py.File, nano5 : h5py.File, fastqs : dict) -> tuple[pd.DataFrame, pd.DataFrame]:
     
     # store key features for each read describing the segmentation and basecalling quality
-    df = pd.DataFrame(
-        columns=['type', 'segment_mean', 'segment_stdev', 'segmentation', 'mean_error', 'stdev_error', 'max_error', 'min_error'],
-        dtype=['str', 'float','float','float','float','float','float','float'])
+    read_statistics = pd.DataFrame(columns=['segmentation type', 'read', 'data Available', 'read segments mean', 'read segments stdev', 'basecall segmentation ratio', 'mean error', 'stdev error', 'max error', 'min error'])
+    read_statistics = read_statistics.astype(dtype = {'segmentation type':'str', 'read':'str', 'data Available':'bool', 'read segments mean':'float', 'read segments stdev':'float', 'basecall segmentation ratio':'float', 'mean error':'float', 'stdev error':'float', 'max error':'float', 'min error':'float'})
 
-    for readid in nano5:
+    # analyse each individual segment
+    segment_statistics = pd.DataFrame(columns=['segmentation type', 'read', 'segment mean', 'segment stdev', 'segment length'])
+    segment_statistics = segment_statistics.astype(dtype = {'segmentation type':'str', 'read':'str', 'segment mean':'float', 'segment stdev':'float', 'segment length':'int'})
+
+    noSeg = 0
+
+    for i, read in enumerate(sim5):
+
+        if not 'read' in read:
+            continue
+
+        print(f'Analysing segmentation of read {read} - {i}/{len(sim5) - 1} ...    ', end='\r')
 
         # key in sim5, ONT FAST5 format
-        read = f'read_{readid}'
+        readid = read.split('_')[-1]
 
-        nanoBorders = nano5[readid][:, 0]
         simBorders = sim5[read]['Raw/Borders'][:]
-        nSimSegments = sim5[read]['Raw'].attrs['num_segments'] # ~ #bases: #bases = #segments + 4, #segments = #5mers
-        signal = sim5[read]['Raw/Signal'][:]
+        nSimSegments = sim5[read]['Raw'].attrs['num_segments']
+        r_offset = sim5[read]['channel_id'].attrs['offset']
+        r_range = sim5[read]['channel_id'].attrs['range']
+        r_digit =sim5[read]['channel_id'].attrs['digitisation']
+        signal = (sim5[read]['Raw/Signal'][:] + r_offset) * r_range / r_digit
         assert (nSimSegments + 1) == len(simBorders)
-        e = getSegmentErrors(nanoBorders, simBorders)
+
+        if readid in nano5:
+            nanoBorders = nano5[readid][:, 0]
+            e = getSegmentErrors(nanoBorders, simBorders)
+        # Nanopolish has no segmentation for this read
+        # maybe read could not be mapped with minimap2
+        else:
+            nanoBorders = [np.nan]
+            e = [np.nan]
+            noSeg += 1
+    
         read : str = str(fastqs[readid].seq)
         # phred : list[int] = fastqs[readid].letter_annotations['phred_quality']
 
         sim_entry = pd.DataFrame({
-            'type':['simulation'],
-            'segment_mean':[np.mean([np.mean(signal[simBorders[i]:simBorders[i+1]]) for i in range(nSimSegments)])],
-            'segment_stdev':[np.mean([np.std(signal[simBorders[i]:simBorders[i+1]]) for i in range(nSimSegments)])],
-            'segmentation':[len(read)/nSimSegments],
-            'mean_error':[np.nan],
-            'stdev_error':[np.nan],
-            'max_error':[np.nan],
-            'min_error':[np.nan]
+            'segmentation type':['simulation'],
+            'read':[readid],
+            'data Available':[True],
+            'read segments mean':[np.mean([np.mean(signal[simBorders[i]:simBorders[i+1]]) for i in range(nSimSegments)])],
+            'read segments stdev':[np.mean([np.std(signal[simBorders[i]:simBorders[i+1]]) for i in range(nSimSegments)])],
+            'basecall segmentation ratio':[len(read)/nSimSegments],
+            'mean error':[np.nan],
+            'stdev error':[np.nan],
+            'max error':[np.nan],
+            'min error':[np.nan]
         })
 
         nano_entry = pd.DataFrame({
-            'type':['nanopolish'],
-            'segment_mean':[np.mean([np.mean(signal[nanoBorders[i]:nanoBorders[i+1]]) for i in range(len(nanoBorders) - 1)])],
-            'segment_stdev':[np.mean([np.std(signal[nanoBorders[i]:nanoBorders[i+1]]) for i in range(len(nanoBorders) - 1)])],
-            'segmentation':[len(read)/(len(simBorders) - 1)],
-            'mean_error':[np.mean(e)],
-            'stdev_error':[np.std(e)],
-            'max_error':[np.max(e)],
-            'min_error':[np.min(e)]
+            'segmentation type':['nanopolish'],
+            'read':[readid],
+            'data Available':[True if len(nanoBorders) > 1 else False],
+            'read segments mean':[np.mean([np.mean(signal[nanoBorders[i]:nanoBorders[i+1]]) for i in range(len(nanoBorders) - 1)]) if len(nanoBorders) > 1 else np.nan],
+            'read segments stdev':[np.mean([np.std(signal[nanoBorders[i]:nanoBorders[i+1]]) for i in range(len(nanoBorders) - 1)]) if len(nanoBorders) > 1 else np.nan],
+            'basecall segmentation ratio':[len(read)/(len(nanoBorders) - 1) if len(nanoBorders) > 1 else np.Infinity],
+            'mean error':[np.mean(e)],
+            'stdev error':[np.std(e)],
+            'max error':[np.max(e)],
+            'min error':[np.min(e)]
         })
-        
-        df = df.append([df, sim_entry, nano_entry], ignore_index=True)
-        
-    return df
 
-def plot(df : pd.DataFrame, path : str) -> None:
+        read_statistics = pd.concat([read_statistics, sim_entry, nano_entry], ignore_index=True)
+
+        if readid in nano5:
+
+            for sim_i in np.random.choice(nSimSegments, size = 100, replace = False): #range(nSimSegments):
+
+                base_sim_entry = pd.DataFrame({
+                    'segmentation type':['simulation'],
+                    'read':[readid],
+                    'segment mean':[np.mean(signal[simBorders[sim_i]:simBorders[sim_i+1]])],
+                    'segment stdev':[np.std(signal[simBorders[sim_i]:simBorders[sim_i+1]])],
+                    'segment length':[simBorders[sim_i+1] - simBorders[sim_i]]
+                })
+
+                segment_statistics = pd.concat([segment_statistics, base_sim_entry], ignore_index=True)
+
+            for nano_i in np.random.choice(len(nanoBorders) - 1, size = 100, replace = False):#range(len(nanoBorders) - 1):
+
+                nano_sim_entry = pd.DataFrame({
+                    'segmentation type':['nanopolish'],
+                    'read':[readid],
+                    'segment mean':[np.mean(signal[nanoBorders[nano_i]:nanoBorders[nano_i+1]])],
+                    'segment stdev':[np.std(signal[nanoBorders[nano_i]:nanoBorders[nano_i+1]])],
+                    'segment length':[nanoBorders[nano_i+1] - nanoBorders[nano_i]]
+                })
+
+                segment_statistics = pd.concat([segment_statistics, nano_sim_entry], ignore_index=True)
+
+    print('Done')
+    print(f'Found {noSeg} reads without nanopolish segmentation')
+
+    return read_statistics, segment_statistics
+
+def plot(read_df : pd.DataFrame, segment_df : pd.DataFrame, path : str) -> None:
     '''
     Plot segmentation quality
     '''
+    #  Read plots
+
     sns.set(style='whitegrid')
-    sns.lmplot(x='segment_stdev', y='mean_error', data=df.loc[df['type'] == 'nanopolish'])
-    plt.savefig(os.path.join(path, 'nano_std_error.png'))
+    sns.scatterplot(x='mean error', y='read segments stdev', data=read_df.loc[read_df['segmentation type'] == 'nanopolish'])
+    ax = plt.gca()
+    ax.set_title('Read mean error vs segment stdev')
+    plt.tight_layout()
+    plt.savefig(os.path.join(path, 'read_nano_std_error.png'))
     plt.close()
     
-    sns.jointplot(data = df, x = 'segment_mean', y = 'segment_stdev', hue = 'type')
-    plt.savefig(os.path.join(path, 'segment_distritbuions.png'))
+    g = sns.jointplot(data = read_df, x = 'read segments mean', y = 'read segments stdev', hue = 'segmentation type')
+    g.fig.suptitle('Mean read segments means vs mean read segments stdevs\nin pA')
+    g.fig.tight_layout()
+    plt.savefig(os.path.join(path, 'read_segment_distributions.png'))
     plt.close()
     
-    sns.jointplot(data = df.loc[df['type'] == 'nanopolish'], x = 'mean_error', y = 'stdev_error')
-    plt.savefig(os.path.join(path, 'nano_error.png'))
+    g = sns.jointplot(data = read_df.loc[read_df['segmentation type'] == 'nanopolish'], x = 'mean error', y = 'stdev error')
+    g.fig.suptitle('Nanopolish segment mean error vs stdev error')
+    g.fig.tight_layout()
+    plt.savefig(os.path.join(path, 'read_nano_error.png'))
     plt.close()
     
-    sns.histplot(data = df, x = 'segmentation', hue = 'type')
-    plt.savefig(os.path.join(path, 'segmentation.png'))
+    sns.histplot(data = read_df, x = 'basecall segmentation ratio', hue = 'segmentation type')
+    ax = plt.gca()
+    ax.set_title('Basecall-Segmentation ratio per segmentation type\n#-bases / #-segments')
+    plt.tight_layout()
+    plt.savefig(os.path.join(path, 'read_segmentation.png'))
     plt.close()
     
-    
+    # Segment plots
+    g = sns.jointplot(data = segment_df, x = 'segment mean', y = 'segment stdev', hue = 'segmentation type')
+    g.fig.suptitle('Segment mean vs segment stdev for all segments\n100 segments per read')
+    g.fig.tight_layout()
+    plt.savefig(os.path.join(path, 'segment_distributions.png'))
+    plt.close()
+
+    sns.histplot(data = segment_df, x = 'segment length', hue = 'segmentation type', bins = 60, stat = 'density')
+    plt.title('Segment length distribution')
+    plt.savefig(os.path.join(path, 'segment_length.png'))
+    plt.close()
+
 # TODO maybe change this to some kind of mapping, use bases/basecalling as index for segments
 def getSegmentErrors(nanoBorders : np.ndarray, simBorders : np.ndarray) -> list:
     '''
@@ -100,11 +179,12 @@ def getSegmentErrors(nanoBorders : np.ndarray, simBorders : np.ndarray) -> list:
     nanoBorders : sorted np.ndarray
     simBorders : sorted np.ndarray
     '''
+
     errors = []
     simIdx = 0
 
     for border in nanoBorders:
-        while(border >= simBorders[simIdx]):
+        while(border >= simBorders[simIdx] and simIdx < len(simBorders)):
             simIdx += 1
             
         ld = border - simBorders[simIdx - 1]
@@ -124,8 +204,14 @@ def main() -> None:
     sim5 = h5py.File(args.sim_fast5, 'r')
     nano5 = h5py.File(args.nanopolish_segmentation_fast5, 'r')
     fq_dict = readFastq(args.fastq)
+    outpath = args.outpath
 
-    df = compare(sim5, nano5, fq_dict)
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
+
+    print('Start comparison')
+    read_df, segment_df = compare(sim5, nano5, fq_dict)
+    plot(read_df, segment_df, outpath)
 
 if __name__ == '__main__':
     main()
